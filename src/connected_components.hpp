d0a62316c5a22509e6d22568d950ec63c0dcfc48
kdeweese <kgd.baf+github@gmail.com>
2023-09-08 11:51:27 -0700

    updated component algorithms

--- src/connected_components.hpp
+++ src/connected_components.hpp
@@ -6,9 +6,9 @@ template <adjacency_list G,
           ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
 requires ranges::random_access_range<vertex_range_t<GT>> && integral<vertex_id_t<GT>>
-void kosaraju(G&&        g,
-              GT&&       g_t,
-              Component& component) {}
+void scc(G&&        g,
+	 GT&&       g_t,
+	 Component& component) {}
 
 /*
  * Tarjan Strongly Connected Components
@@ -16,5 +16,36 @@ void kosaraju(G&&        g,
 template <adjacency_list G,
           ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void tarjan_cc(G&&        g,
-               Component& component) {}
+void scc(G&&        g,
+	 Component& component) {}
+
+/*
+ * Hopcroft_Tarjan Articulation Points
+ */
+template <adjacency_list G,
+          class Iter>
+requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
+         std::output_iterator<Iter, vertex_id_t<G>>
+void articulation_points(G&&  g,
+                         Iter cut_vertices) {}
+/*
+ * Hopcroft-Tarjan Biconnected Components
+ */
+template <adjacency_list G,
+          class Container = std::set<vertex_id_t<G>>,
+          class InnerIter = std::insert_iterator<Container>,
+          class OuterIter >
+requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
+         std::output_iterator<OuterIter, Container>
+         && std::output_iterator<InnerIter, vertex_id_t<G>>
+void bicc(G&&       g,
+	  OuterIter component) {}
+
+/*
+ * Connected Components
+ */
+template <adjacency_list G,
+          ranges::random_access_range Component>
+requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
+void cc(G&&        g,
+	Component& component) {}
