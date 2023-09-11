8bbace910b5bbd61d3e6a6f50f85e30eb9195f29
kdeweese <kgd.baf+github@gmail.com>
2023-09-10 20:53:57 -0700

    updated components section with lums feedback

--- src/connected_components.hpp
+++ src/connected_components.hpp
@@ -5,10 +5,10 @@ template <adjacency_list G,
           adjacency_list GT,
           ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
-requires ranges::random_access_range<vertex_range_t<GT>> && integral<vertex_id_t<GT>>
-void scc(G&&        g,
-	 GT&&       g_t,
-	 Component& component) {}
+         ranges::random_access_range<vertex_range_t<GT>> && integral<vertex_id_t<GT>>
+void strongly_connected_components(G&&        g,
+				   GT&&       g_t,
+				   Component& component) {}
 
 /*
  * Tarjan Strongly Connected Components
@@ -16,8 +16,8 @@ void scc(G&&        g,
 template <adjacency_list G,
           ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void scc(G&&        g,
-	 Component& component) {}
+void strongly_connected_components(G&&        g,
+	                           Component& component) {}
 
 /*
  * Hopcroft_Tarjan Articulation Points
@@ -32,14 +32,12 @@ void articulation_points(G&&  g,
  * Hopcroft-Tarjan Biconnected Components
  */
 template <adjacency_list G,
-          class Container = std::set<vertex_id_t<G>>,
-          class InnerIter = std::insert_iterator<Container>,
-          class OuterIter >
+          std::ranges::forward_range OuterContainer>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>> &&
-         std::output_iterator<OuterIter, Container>
-         && std::output_iterator<InnerIter, vertex_id_t<G>>
-void bicc(G&&       g,
-	  OuterIter component) {}
+         std::ranges::forward_range<std::ranges::range_value_t<OuterContainer>> &&
+         integral<std::ranges::forward_range_t<std::ranges::forward_range_t<OuterContainer>>>
+void biconnected_components(G&&             g,
+			    OuterContainer& components) {}
 
 /*
  * Connected Components
@@ -47,5 +45,5 @@ void bicc(G&&       g,
 template <adjacency_list G,
           ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void cc(G&&        g,
-	Component& component) {}
+void connected_components(G&&        g,
+			  Component& component) {}
