ba7d6313047c2e7de20725b268f270b2e6edd791
neoblizz <osama94@gmail.com>
2023-10-30 13:19:10 -0700

    Relabel.

---
+++ src/operators/relabel.hpp
@@ -0,0 +1,7 @@
+template <class ExecutionPolicy,
+          adjacency_list_graph G>
+void relabel(ExecutionPolicy&& policy, G& graph, const std::map</*?*/, /*?*/>& mapping) {}
+
+template <class ExecutionPolicy,
+          adjacency_list_graph G>
+void relabel_to_integers(ExecutionPolicy&& policy, G& graph) {}
