ebdaf22152879c62889d9fa8d8a921bb1a1dc0d7
neoblizz <osama94@gmail.com>
2023-10-30 15:19:06 -0700

    ExecutionPolicy removed.

--- src/operators/sort.hpp
+++ src/operators/sort.hpp
@@ -1,41 +1,33 @@
 // TODO: requires sourced_adjacency_list?
-template <class ExecutionPolicy, 
-          adjacency_list_graph G>
+template <adjacency_list_graph G>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_source(ExecutionPolicy&& policy, G& graph) {}
+void sort_by_source(G& graph) {}
 
-template <class ExecutionPolicy, 
-          adjacency_list_graph G>
+template <adjacency_list_graph G>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_target(ExecutionPolicy&& policy, G& graph) {}
+void sort_by_target(G& graph) {}
 
-template <class ExecutionPolicy, 
-          adjacency_list_graph G>
-void sort_by_degree(ExecutionPolicy&& policy, G& graph) {}
+template <adjacency_list_graph G>
+void sort_by_degree(G& graph) {}
 
-template <class ExecutionPolicy, 
-          adjacency_list_graph G,
+template <adjacency_list_graph G,
           class W>
 requires weight_function<W, edge_t<G>> && ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_weight(ExecutionPolicy&& policy, G& graph,  W&& w) {}
+void sort_by_weight(G& graph,  W&& w) {}
 
 // Needs the same functionality for edge_list_graph (requires edgelist_range_t?)
-template <class ExecutionPolicy, 
-          edge_list_graph G>
+template <edge_list_graph G>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_source(ExecutionPolicy&& policy, G& graph) {}
+void sort_by_source(G& graph) {}
 
-template <class ExecutionPolicy, 
-          edge_list_graph G>
+template <edge_list_graph G>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_target(ExecutionPolicy&& policy, G& graph) {}
+void sort_by_target(G& graph) {}
 
-template <class ExecutionPolicy, 
-          edge_list_graph G>
-void sort_by_degree(ExecutionPolicy&& policy, G& graph) {}
+template <edge_list_graph G>
+void sort_by_degree(G& graph) {}
 
-template <class ExecutionPolicy, 
-          edge_list_graph G,
+template <edge_list_graph G,
           class W>
 requires weight_function<W, edge_t<G>> && ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
-void sort_by_weight(ExecutionPolicy&& policy, G& graph,  W&& w) {}
+void sort_by_weight(G& graph,  W&& w) {}
