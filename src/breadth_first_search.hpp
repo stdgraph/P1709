776d47ef3f70988341c4e22178d5df1bf6c24b05
Phil Ratzloff <pratzl@outlook.com>
2023-12-03 14:13:17 -0500

    Remove BFS prototypes for ExecutionPolicy

--- src/breadth_first_search.hpp
+++ src/breadth_first_search.hpp
@@ -8,33 +8,9 @@ template <adjacency_list_graph G, property D, property P, queueable Q>
 void breadth_first_search(
       const G& graph, vertex_id_t<G> source, P&& predecessors, D&& distances, Q&& q);
 
-template <class ExecutionPolicy, adjacency_list_graph G, property D, property P>
-void breadth_first_search(ExecutionPolicy&& policy,
-                          const G&          graph,
-                          vertex_id_t<G>    source,
-                          P&&               predecessors,
-                          D&&               distances);
-
-template <class ExecutionPolicy, adjacency_list_graph G, property P, property D, queueable Q>
-void breadth_first_search(ExecutionPolicy&& policy,
-                          const G&          graph,
-                          vertex_id_t<G>    source,
-                          P&&               predecessors,
-                          D&&               distances,
-                          Q&&               q);
-
 template <adjacency_list_graph G, property D>
 void breadth_first_distances(const G& graph, vertex_id_t<G> source, D&& distances);
 
 template <adjacency_list_graph G, property D, queueable Q>
 void breadth_first_distances(const G& graph, vertex_id_t<G> source, D&& distances, Q&& q);
 
-template <class ExecutionPolicy, adjacency_list_graph G, property D, property P>
-void breadth_first_distances(ExecutionPolicy&& policy,
-                             const G&          graph,
-                             vertex_id_t<G>    source,
-                             D&&               distances);
-
-template <class ExecutionPolicy, adjacency_list_graph G, property D, queueable Q>
-void breadth_first_distances(
-      ExecutionPolicy&& policy, const G& graph, vertex_id_t<G> source, D&& distances, Q&& q);
