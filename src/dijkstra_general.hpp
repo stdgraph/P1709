ffe6697b6f2e3e4102a928fb3be055feea4285b3
Phil Ratzloff <pratzl@outlook.com>
2023-12-21 17:07:56 -0500

    Update dijkstra general algorithms with implementation
    Copy current dijkstra to bellman_ford prototypes

--- src/dijkstra_general.hpp
+++ src/dijkstra_general.hpp
@@ -1,30 +1,40 @@
-template <index_adjacency_list G, 
-          random_access_range  Distances,
-          random_access_range  Predecessors,
-          class                Compare, 
-          class                Combine,
-          class WF = function<range_value_t<Distances>(edge_reference_t<G>)>>
-requires convertible_to<vertex_id_t<G>, range_value_t<Predecessor>> &&
-         basic_edge_weight_function<G, WF, range_value_t<Distance>, Compare, Combine>
+template <index_adjacency_list        G,
+          ranges::random_access_range Distances,
+          ranges::random_access_range Predecessors,
+          class Compare,
+          class Combine,
+          class WF        = function<ranges::range_value_t<Distances>(edge_reference_t<G>)>,
+          class Allocator = allocator<vertex_id_t<G>>
+          >
+requires is_arithmetic_v<ranges::range_value_t<Distances>> &&
+         convertible_to<vertex_id_t<G>, ranges::range_value_t<Predecessors>> &&
+         basic_edge_weight_function<G, WF, ranges::range_value_t<Distances>, Compare, Combine>
 void dijkstra_shortest_paths(
-      const G&       graph, 
-      vertex_id_t<G> source, 
-      Distances&     distances, 
-      Predecessors&  predecessors, 
+      G&&            g,            // graph
+      vertex_id_t<G> source,       // starting vertex\_id
+      Distances&     distances,    // out: Distances[uid] of uid from source
+      Predecessors&  predecessors, // out: predecessor[uid] of uid in path
       Compare&&      compare,
       Combine&&      combine,
-      WF&& w = [](edge_reference_t<G> uv) { return range_value_t<Distance>(1); });
+      WF&&           weight = // default weight(uv) -> 1
+            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); },
+      Allocator alloc = Allocator());
 
-template <index_adjacency_list G, 
-          random_access_range  Distances,
-          class                Compare, 
-          class                Combine,
-          class WF = function<range_value_t<Distances>(edge_reference_t<G>)>>
-requires basic_edge_weight_function<G, WF, range_value_t<Distances>, Compare, Combine>
+template <index_adjacency_list        G,
+          ranges::random_access_range Distances,
+          class Compare,
+          class Combine,
+          class WF        = std::function<ranges::range_value_t<Distances>(edge_reference_t<G>)>,
+          class Allocator = allocator<vertex_id_t<G>>
+          >
+requires is_arithmetic_v<ranges::range_value_t<Distances>> &&
+         basic_edge_weight_function<G, WF, ranges::range_value_t<Distances>, Compare, Combine>
 void dijkstra_shortest_distances(
-      const G&       graph, 
-      vertex_id_t<G> source, 
-      Distance&      distances, 
+      G&&            g,         // graph
+      vertex_id_t<G> seed,      // starting vertex\_id
+      Distances&     distances, // out: Distances[uid] of uid from seed
       Compare&&      compare,
       Combine&&      combine,
-      WF&& w = [](edge_reference_t<G> uv) { return range_value_t<Distance>(1); });
+      WF&&           weight = // default weight(uv) -> 1
+            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); },
+      Allocator alloc = Allocator());
