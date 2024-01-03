8d72189fb019835db985c8931ea2c692df3f08cf
Phil Ratzloff <pratzl@outlook.com>
2024-01-03 15:50:25 -0500

    Refinement edits
    Update breadth_first_search algorithm to be near Dijkstra (for review)
    Add \phil comments to jaccard
    Add \phil suggestion to add Adamic-Adar index to Tier2
    
    Conventions:
    add EId type w/ eid and uvid variables
    Add vvf(uid) overload
    Add evf(eid) overload
    
    Views
    Add vvf(uid) overload for basic_ views
    Add evf(eid) overload for basic_ views

--- src/breadth_first_search.hpp
+++ src/breadth_first_search.hpp
@@ -1,16 +1,15 @@
-template <adjacency_list_graph G, property D, property P>
+template <index_adjacency_list G, property D, property P>
 void breadth_first_search(const G&       graph,
                           vertex_id_t<G> source,
                           P&&            predecessors,
                           D&&            distances);
 
-template <adjacency_list_graph G, property D, property P, queueable Q>
+template <index_adjacency_list G, property D, property P, queueable Q>
 void breadth_first_search(
       const G& graph, vertex_id_t<G> source, P&& predecessors, D&& distances, Q&& q);
 
-template <adjacency_list_graph G, property D>
+template <index_adjacency_list G, property D>
 void breadth_first_distances(const G& graph, vertex_id_t<G> source, D&& distances);
 
-template <adjacency_list_graph G, property D, queueable Q>
+template <index_adjacency_list G, property D, queueable Q>
 void breadth_first_distances(const G& graph, vertex_id_t<G> source, D&& distances, Q&& q);
-
--- src/jaccard.hpp
+++ src/jaccard.hpp
@@ -2,5 +2,5 @@
  * Jaccard Coefficient Algorithm
  */
 template <index_adjacency_list G, typename OutOp, typename T = double>
-requires is_invocable_v<OutOp, vertex_id_t<G>&, vertex_id_t<G>&, edge_t<G>&, T>
+requires is_invocable_v<OutOp, vertex_id_t<G>&, vertex_id_t<G>&, edge_reference_t<G>, T>
 void jaccard_coefficient(G&& g, OutOp out) {}
