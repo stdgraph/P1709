ff7491759d5f5859fa3dccdf560edcbb7a81e79e
Phil Ratzloff <pratzl@outlook.com>
2024-01-06 15:19:39 -0500

    Add topological sort definitions
    Add comments for Other Algorithms and Transpose

--- src/breadth_first_search.hpp
+++ src/breadth_first_search.hpp
@@ -1,4 +1,5 @@
-template <index_adjacency_list G, class Predecessors>
+template <index_adjacency_list G, class Predecessors, class Allocator = allocator<vertex_id_t<G>>
 void breadth_first_search(const G&       graph,
                           vertex_id_t<G> source,
-                          Predecessors&  predecessors);
+                          Predecessors&  predecessors,
+                          Allocator      alloc = Allocator());
