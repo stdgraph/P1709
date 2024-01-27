8e4d2cecb901461e0660c730a71823960de9cb2c
Phil Ratzloff <pratzl@outlook.com>
2024-01-27 14:38:36 -0500

    Replace concepts summary table with code
    Added sourced_targeted_edge & target_edge_range
    
    Make sure all combinations of {basic, sourced, index} combinations exist for adj list concepts.

---
+++ D9905/src/concepts_vertex_range.hpp
@@ -0,0 +1,12 @@
+template <class G> // (exposition only)
+concept _common_vertex_range = ranges::sized_range<vertex_range_t<G>> && //
+                               requires(G&& g, vertex_iterator_t<G> ui) { vertex_id(g, ui); };
+
+template <class G>
+concept vertex_range = _common_vertex_range<vertex_range_t<G>> && //
+                       ranges::forward_range<vertex_range_t<G>>;
+
+template <class G>
+concept index_vertex_range = _common_vertex_range<vertex_range_t<G>> &&        //
+                             ranges::random_access_range<vertex_range_t<G>> && //
+                             integral<vertex_id_t<G>>;
