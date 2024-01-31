6e24ef6b70070b143a7d4e8ebc9ff82c1e27d9ce
Phil Ratzloff <pratzl@outlook.com>
2024-01-31 17:53:51 -0500

    Remove allocators from algorithms (for now)

--- D9902/src/bellman_ford_common.hpp
+++ D9902/src/bellman_ford_common.hpp
@@ -2,7 +2,6 @@ template <index_adjacency_list        G,
           ranges::random_access_range Distances,
           ranges::random_access_range Predecessors,
           class WF        = function<ranges::range_value_t<Distances>(edge_reference_t<G>)>
-          class Allocator = allocator<vertex_id_t<G>>
           >
 requires is_arithmetic_v<ranges::range_value_t<Distances>> &&
          convertible_to<vertex_id_t<G>, ranges::range_value_t<Predecessors>> &&
@@ -13,13 +12,11 @@ void bellman_ford_shortest_paths(
       Distances&     distances,    // out: Distances[uid] of uid from source
       Predecessors&  predecessors, // out: predecessor[uid] of uid in path
       WF&&           weight =
-            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); },
-      Allocator alloc = Allocator())                                                  
+            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); })                                                  
 
 template <index_adjacency_list        G,
           ranges::random_access_range Distances,
-          class WF        = function<ranges::range_value_t<Distances>(edge_reference_t<G>)>,
-          class Allocator = allocator<vertex_id_t<G>>
+          class WF        = function<ranges::range_value_t<Distances>(edge_reference_t<G>)>
           >
 requires is_arithmetic_v<ranges::range_value_t<Distances>> &&
          edge_weight_function<G, WF, ranges::range_value_t<Distances>>
@@ -28,5 +25,4 @@ void bellman_ford_shortest_distances(
       vertex_id_t<G> seed,      // starting vertex\_id
       Distances&     distances, // out: Distances[uid] of uid from seed
       WF&&           weight =
-            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); },
-      Allocator alloc = Allocator());
+            [](edge_reference_t<G> uv) { return ranges::range_value_t<Distances>(1); });
