c564b6b8d1c124668d3d78533d99a91f754b9a7c
Phil Ratzloff <pratzl@outlook.com>
2024-01-30 20:44:26 -0500

    Rename concepts based on input
    Move container data structure info from GCI to Containers paper

--- D9905/src/concepts_basic_adj_list.hpp
+++ D9905/src/concepts_basic_adj_list.hpp
@@ -1,17 +1,18 @@
 template <class G>
-concept basic_adjacency_list = vertex_range<G> &&            //
-                               basic_target_edge_range<G> && //
+concept basic_adjacency_list = vertex_range<G> &&              //
+                               basic_targeted_edge_range<G> && //
                                targeted_edge<G, edge_t<G>>;
 
 template <class G>
-concept basic_index_adjacency_list = index_vertex_range<G> &&      //
-                                     basic_target_edge_range<G> && //
-                                     targeted_edge<G, edge_t<G>>;
+concept basic_index_adjacency_list = index_vertex_range<G> &&        //
+                                     basic_targeted_edge_range<G> && //
+                                     basic_targeted_edge<G, edge_t<G>>;
+template <class G>
+concept basic_sourced_adjacency_list = vertex_range<G> &&              //
+                                       basic_targeted_edge_range<G> && //
+                                       basic_sourced_targeted_edge<G, edge_t<G>>;
 
 template <class G>
-concept basic_sourced_adjacency_list = basic_adjacency_list<G> && //
-                                       sourced_targeted_edge<G, edge_t<G>>;
-
-template <class G>
-concept basic_sourced_index_adjacency_list = basic_index_adjacency_list<G> && //
-                                             sourced_targeted_edge<G, edge_t<G>>;
+concept basic_sourced_index_adjacency_list = index_vertex_range<G> &&        //
+                                             basic_targeted_edge_range<G> && //
+                                             basic_sourced_targeted_edge<G, edge_t<G>>;
--- D9905/src/concepts_target_edge_range.hpp
+++ D9905/src/concepts_target_edge_range.hpp
@@ -1,10 +1,10 @@
 template <class G>
-concept basic_target_edge_range = requires(G&& g, vertex_id_t<G> uid) {
+concept basic_targeted_edge_range = requires(G&& g, vertex_id_t<G> uid) {
   { edges(g, uid) } -> ranges::forward_range;
 };
 
 template <class G>
-concept target_edge_range = basic_target_edge_range<G> && //
-                            requires(G&& g, vertex_reference_t<G> u) {
-                              { edges(g, u) } -> ranges::forward_range;
-                            };
+concept targeted_edge_range = basic_targeted_edge_range<G> && //
+                              requires(G&& g, vertex_reference_t<G> u) {
+                                { edges(g, u) } -> ranges::forward_range;
+                              };
