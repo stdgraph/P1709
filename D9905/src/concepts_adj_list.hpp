7fe3f1bd1a60556e4d26b84d9f22609722258004
Phil Ratzloff <pratzl@outlook.com>
2024-01-30 20:44:26 -0500

    Rename concepts based on input
    Move container data structure info from GCI to Containers paper

--- D9905/src/concepts_adj_list.hpp
+++ D9905/src/concepts_adj_list.hpp
@@ -1,16 +1,18 @@
 template <class G>
-concept adjacency_list = vertex_range<G> &&      //
-                         target_edge_range<G> && //
+concept adjacency_list = vertex_range<G> &&        //
+                         targeted_edge_range<G> && //
                          targeted_edge<G, edge_t<G>>;
 
 template <class G>
-concept index_adjacency_list = index_vertex_range<G> && //
-                               target_edge_range<G> &&  //
+concept index_adjacency_list = index_vertex_range<G> &&  //
+                               targeted_edge_range<G> && //
                                targeted_edge<G, edge_t<G>>;
 template <class G>
-concept sourced_adjacency_list = adjacency_list<G> && //
+concept sourced_adjacency_list = vertex_range<G> &&        //
+                                 targeted_edge_range<G> && //
                                  sourced_targeted_edge<G, edge_t<G>>;
 
 template <class G>
-concept sourced_index_adjacency_list = index_adjacency_list<G> && //
+concept sourced_index_adjacency_list = index_vertex_range<G> &&  //
+                                       targeted_edge_range<G> && //
                                        sourced_targeted_edge<G, edge_t<G>>;
--- D9905/src/concepts_edges.hpp
+++ D9905/src/concepts_edges.hpp
@@ -1,15 +1,22 @@
 template <class G, class E>
-concept targeted_edge = requires(G&& g, edge_reference_t<G> uv) {
-  target_id(g, uv);
-  target(g, uv);
-};
+concept basic_targeted_edge = requires(G&& g, edge_reference_t<G> uv) { target_id(g, uv); };
 
 template <class G, class E>
-concept sourced_edge = requires(G&& g, edge_reference_t<G> uv) {
-  source_id(g, uv);
-  source(g, uv);
-};
+concept basic_sourced_edge = requires(G&& g, edge_reference_t<G> uv) { source_id(g, uv); };
 
 template <class G, class E>
-concept sourced_targeted_edge = targeted_edge<G, E> && sourced_edge<G, E> && //
+concept basic_sourced_targeted_edge = basic_targeted_edge<G, E> && //
+                                      basic_sourced_edge<G, E> &&  //
+                                      requires(G&& g, edge_reference_t<G> uv) { edge_id(g, uv); };
+template <class G, class E>
+concept targeted_edge = basic_targeted_edge<G, E> && //
+                        requires(G&& g, edge_reference_t<G> uv) { target(g, uv); };
+
+template <class G, class E>
+concept sourced_edge = basic_sourced_edge<G, E> && //
+                       requires(G&& g, edge_reference_t<G> uv) { source(g, uv); };
+
+template <class G, class E>
+concept sourced_targeted_edge = targeted_edge<G, E> && //
+                                sourced_edge<G, E> &&  //
                                 requires(G&& g, edge_reference_t<G> uv) { edge_id(g, uv); };
