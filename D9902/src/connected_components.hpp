74da5c2dee7055f566871c59d08365c605320578
Phil Ratzloff <pratzl@outlook.com>
2024-01-31 17:53:51 -0500

    Remove allocators from algorithms (for now)

--- D9902/src/connected_components.hpp
+++ D9902/src/connected_components.hpp
@@ -1,52 +1,43 @@
 /*
  * Hopcroft-Tarjan Articulation Points
  */
-template <index_adjacency_list G, class Iter, class Allocator = allocator<vertex_id<G>>>
+template <index_adjacency_list G, class Iter>
 requires output_iterator<Iter, vertex_id_t<G>>
-void articulation_points(G&&  g, Iter cut_vertices, Allocator alloc = Allocator());
+void articulation_points(G&&  g, Iter cut_vertices);
 
 /*
  * Hopcroft-Tarjan Biconnected Components
  */
 template <index_adjacency_list G,
-          ranges::forward_range OuterContainer, 
-          class Allocator = allocator<vertex_id<G>>
+          ranges::forward_range OuterContainer>
 requires ranges::forward_range<ranges::range_value_t<OuterContainer>> &&
          integral<ranges::forward_range_t<ranges::forward_range_t<OuterContainer>>>
 void biconnected_components(G&&             g,
-			    OuterContainer& components,
-                            Allocator alloc = Allocator());
+			    OuterContainer& components);
 
 /*
  * Connected Components
  */
 template <index_adjacency_list G,
-          ranges::random_access_range Component,
-          class Allocator = allocator<vertex_id<G>>
+          ranges::random_access_range Component>
 void connected_components(G&&        g,
-			  Component& component,
-                          Allocator alloc = Allocator());
+			  Component& component);
 
 /*
  * Kosaraju Strongly Connected Components
  */
 template <index_adjacency_list G,
           index_adjacency_list GT,
-          ranges::random_access_range Component,
-          class Allocator = allocator<vertex_id<G>>
+          ranges::random_access_range Component>
 void strongly_connected_components(G&&        g,
 				   GT&&       g_t,
-				   Component& component,
-                                   Allocator alloc = Allocator());
+				   Component& component);
 
 /*
  * Tarjan Strongly Connected Components
  */
 template <adjacency_list G,
-          ranges::random_access_range Component,
-          class Allocator = allocator<vertex_id<G>>>
+          ranges::random_access_range Component>
 requires ranges::random_access_range<vertex_range_t<G>> && integral<vertex_id_t<G>>
 void strongly_connected_components(G&&        g,
-	                          Component& component
-                                  Allocator alloc = Allocator());
-
+	                          Component& component);
