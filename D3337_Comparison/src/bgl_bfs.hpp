e479bf4f140103b1a94f95148a32aef60bd1e35c
kdeweese <kgd.baf+github@gmail.com>
2025-01-07 20:08:46 -0800

    add barebones for D3337 comparison paper

---
+++ D3337_Comparison/src/bgl_bfs.hpp
@@ -0,0 +1,15 @@
+using namespace boost;
+
+using G =
+  compressed_sparse_row_graph<directedS, no_property, no_property>;
+using VId = graph_traits<G>::vertex_descriptor;
+
+G g;
+//populate g
+
+vector<VId> parents(num_vertices(g));
+
+auto vis = make_bfs_visitor(
+  make_pair(record_predecessors(parents.begin(), on_tree_edge())));
+
+breadth_first_search(g, vertex(0, g), visitor(vis));
