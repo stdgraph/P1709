77a59ff730b04dea07a369c9ba56ec06de06dbf9
Phil Ratzloff <pratzl@outlook.com>
2025-03-29 19:53:00 -0400

    Update Views for descriptors

--- D3129_Views/src/vertex_info.hpp
+++ D3129_Views/src/vertex_info.hpp
@@ -1,10 +1,8 @@
-template <class VId, class V, class VV>
+template <class VorVId, class V, class VV>
 struct vertex_info {
-  using id_type     = VId; // e.g. vertex\_id\_t<G>
-  using vertex_type = V;   // e.g. vertex\_reference\_t<G> or void
-  using value_type  = VV;  // e.g. vertex\_value\_t<G> or void
+  using vertex_type = VorVId; // e.g. vertex\_reference\_t<G> or void
+  using value_type  = VV;     // e.g. vertex\_value\_t<G> or void
 
-  id_type     id;
   vertex_type vertex;
   value_type  value;
 };
