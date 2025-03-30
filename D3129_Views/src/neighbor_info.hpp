37904b6bcd4db34b7a5e3d4f5a69bae79247f6c1
Phil Ratzloff <pratzl@outlook.com>
2025-03-29 23:09:48 -0400

    Various refinements basic on descriptors

--- D3129_Views/src/neighbor_info.hpp
+++ D3129_Views/src/neighbor_info.hpp
@@ -1,8 +1,8 @@
-template <class V, bool Sourced, class VV>
+template <class VorVId, bool Sourced, class VV>
 struct neighbor_info {
-  using source_type = V;   // e.g. vertex\_t<G> or vertex\_id\_t<G> when Sourced==true, or void
-  using target_type = V;   // e.g. vertex\_t<G> or vertex\_id\_t<G>
-  using value_type  = VV;  // e.g. vertex\_value\_t<G> or void
+  using source_type = VorVId; // e.g. vertex\_t<G> or vertex\_id\_t<G> when Sourced==true, or void
+  using target_type = VorVId; // e.g. vertex\_t<G> or vertex\_id\_t<G>
+  using value_type  = VV;     // e.g. vertex\_value\_t<G> or void
 
   source_type source;
   target_type target;
--- D3129_Views/src/vertex_info.hpp
+++ D3129_Views/src/vertex_info.hpp
@@ -1,4 +1,4 @@
-template <class VorVId, class V, class VV>
+template <class VorVId, class VV>
 struct vertex_info {
   using vertex_type = VorVId; // e.g. vertex\_reference\_t<G> or void
   using value_type  = VV;     // e.g. vertex\_value\_t<G> or void
