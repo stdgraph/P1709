a1a6d729c9bfa70c6cdcb8da494af898861d6e2c
Phil Ratzloff <pratzl@outlook.com>
2025-04-01 19:46:27 -0400

    Remove "basic" forms of concepts as part of descriptors addition
    Update revision.tex to reflect changes

--- D3130_Container_Interface/src/concepts_target_edge_range.hpp
+++ D3130_Container_Interface/src/concepts_target_edge_range.hpp
@@ -1,12 +1,9 @@
 // For exposition only
 
 template <class G>
-concept basic_targeted_edge_range = requires(G&& g, vertex_id_t<G> uid) {
-  { edges(g, uid) } -> ranges::forward_range;
-};
-
-template <class G>
-concept targeted_edge_range = basic_targeted_edge_range<G> && //
-                              requires(G&& g, vertex_reference_t<G> u) {
-                                { edges(g, u) } -> ranges::forward_range;
-                              };
+concept targeted_edge_range = 
+          basic_targeted_edge_range<G> &&
+          requires(G&& g, vertex_reference_t<G> u, vertex_id_t<G> uid) {
+            { edges(g, u) } -> ranges::forward_range;
+            { edges(g, uid) } -> ranges::forward_range; // implies call to find\_vertex(g,uid)
+          };
--- D3130_Container_Interface/src/concepts_vertex_range.hpp
+++ D3130_Container_Interface/src/concepts_vertex_range.hpp
@@ -2,7 +2,7 @@
 
 template <class G>                                                       // (exposition only)
 concept _common_vertex_range = ranges::sized_range<vertex_range_t<G>> && //
-                               requires(G&& g, vertex_iterator_t<G> ui) { vertex_id(g, ui); };
+                               requires(G&& g, vertex_iterator_t<G> ui) { vertex_id(g, u); };
 
 template <class G>                                                
 concept vertex_range = _common_vertex_range<vertex_range_t<G>> && //
