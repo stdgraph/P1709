fc4a83b1ec2219da5cf395517c1dec844714d57b
Phil Ratzloff <pratzl@outlook.com>
2025-04-01 20:01:14 -0400

    Lesser changes for consistency and clarity

--- D3130_Container_Interface/src/concepts_vertex_range.hpp
+++ D3130_Container_Interface/src/concepts_vertex_range.hpp
@@ -1,14 +1,14 @@
 // For exposition only
 
-template <class G>                                                       // (exposition only)
-concept _common_vertex_range = ranges::sized_range<vertex_range_t<G>> && //
-                               requires(G&& g, vertex_iterator_t<G> ui) { vertex_id(g, u); };
+template <class G>
+concept _common_vertex_range = ranges::sized_range<vertex_range_t<G>> &&
+                                    requires(G&& g, vertex_reference_t<G> u) { vertex_id(g, u); };
 
 template <class G>                                                
-concept vertex_range = _common_vertex_range<vertex_range_t<G>> && //
-                       ranges::forward_range<vertex_range_t<G>>;
+concept vertex_range = _common_vertex_range<vertex_range_t<G>> &&
+                            ranges::forward_range<vertex_range_t<G>>;
 
 template <class G>                                                             
-concept index_vertex_range = _common_vertex_range<vertex_range_t<G>> &&        //
-                             ranges::random_access_range<vertex_range_t<G>> && //
-                             integral<vertex_id_t<G>>;
+concept index_vertex_range = _common_vertex_range<vertex_range_t<G>> &&
+                                ranges::random_access_range<vertex_range_t<G>> &&
+                                integral<vertex_id_t<G>>;
