dfbba38819050fe588ef3868c5bf05d281016cd3
Phil Ratzloff <pratzl@outlook.com>
2025-05-25 15:32:27 -0400

    Feedback from AndrzejK
    Mandates only applies to statically checked compile-time items
    
    New "Hardened preconditions" clause added. Many items in mandates and
    precondications are moved into that. Thows clauses removed.
    
    Weight function name changed from "w" to "weight" to reflect code.
    
    Rename "seed" to "source" to match statements.
    
    Summarize changes in Revisions

--- D3128_Algorithms/src/mst.hpp
+++ D3128_Algorithms/src/mst.hpp
@@ -13,7 +13,7 @@ void kruskal(IELR&& e, OELR&& t, CompareOp compare);
 template <index_adjacency_list        G,
           ranges::random_access_range Predecessor,
           ranges::random_access_range Weight>
-void prim(G&& g, Predecessor& predecessor, Weight& weight, vertex_id_t<G> seed = 0);
+void prim(G&& g, Predecessor& predecessor, Weight& weight, vertex_id_t<G> source = 0);
 
 template <index_adjacency_list        G,
           ranges::random_access_range Predecessor,
@@ -24,4 +24,4 @@ void prim(G&&                           g,
           Weight&                       weight,
           CompareOp                     compare,
           ranges::range_value_t<Weight> init_dist,
-          vertex_id_t<G>                seed = 0);
+          vertex_id_t<G>                source = 0);
--- D3128_Algorithms/src/prim.hpp
+++ D3128_Algorithms/src/prim.hpp
@@ -1,7 +1,7 @@
 template <index_adjacency_list        G,
           ranges::random_access_range Predecessor,
           ranges::random_access_range Weight>
-void prim(G&& g, Predecessor& predecessor, Weight& weight, vertex_id_t<G> seed = 0) {}
+void prim(G&& g, Predecessor& predecessor, Weight& weight, vertex_id_t<G> source = 0) {}
 
 template <index_adjacency_list        G,
           ranges::random_access_range Predecessor,
@@ -12,4 +12,4 @@ void prim(G&&                           g,
           Weight&                       weight,
           CompareOp                     compare,
           ranges::range_value_t<Weight> init_dist,
-          vertex_id_t<G>                seed = 0) {}
+          vertex_id_t<G>                source = 0) {}
