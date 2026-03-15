3c83c4b0b28a6d9c6fda7fbd7317e4a8d27554fe
Phil Ratzloff <pratzl@outlook.com>
2026-03-14 21:18:29 -0400

    D3128: Update Prim's algorithm to match current implementation
    - Consolidate two prim() overloads into a single signature with defaulted
      WF and CompareOp template parameters (seed moved to second parameter)
    - Remove init_dist parameter; add explicit requires clause for
      basic_edge_weight_function with plus<> combine
    - Update mst.hpp and prim.hpp signatures accordingly
    - algorithms.tex: resolve \phil note by documenting delegation to
      dijkstra_shortest_paths; fix lstinputlisting line range (24-39);
      correct Mandates type (vertex_property_map_value_t<Weight>); add
      init_shortest_paths precondition; update Effects; add Throws section;
      add Remarks for disconnected-graph usage"

--- D3128_Algorithms/src/mst.hpp
+++ D3128_Algorithms/src/mst.hpp
@@ -21,23 +21,19 @@ auto inplace_kruskal(IELR&& e, OELR&& t, CompareOp compare);
 /*
  * Prim's Algorithm
  */
-template <adjacency_list G, class Predecessor, class Weight>
-auto prim(G&& g, Predecessor& predecessor, Weight& weight, const vertex_id_t<G>& seed = 0);
-
 template <adjacency_list G,
           class          Predecessor,
           class          Weight,
-          class CompareOp,
-          class WF>
-requires basic_edge_weight_function<G,
-                                    WF,
-                                    vertex_property_map_value_t<Weight>,
-                                    CompareOp,
+          class WF      = function<vertex_property_map_value_t<Weight>(const remove_reference_t<G>&,
+                                                                        const edge_t<G>&)>,
+          class CompareOp = less<vertex_property_map_value_t<Weight>>>
+requires vertex_property_map_for<Predecessor, G> &&
+         vertex_property_map_for<Weight, G> &&
+         basic_edge_weight_function<G, WF, vertex_property_map_value_t<Weight>, CompareOp,
                                     plus<vertex_property_map_value_t<Weight>>>
-auto prim(G&&                                 g,
-          Predecessor&                        predecessor,
-          Weight&                             weight,
-          CompareOp                           compare,
-          vertex_property_map_value_t<Weight> init_dist,
-          WF&&                                weight_fn,
-          vertex_id_t<G>                      seed = 0);
+auto prim(G&&                   g,
+          const vertex_id_t<G>& seed,
+          Predecessor&          predecessor,
+          Weight&               weight,
+          WF&&                  weight_fn = [](const auto&, const edge_t<G>& uv) { return edge_value(g, uv); },
+          CompareOp             compare   = less<vertex_property_map_value_t<Weight>>());
