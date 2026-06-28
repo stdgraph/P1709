9d9397e2d0688644ed695ecaf025d21fb2d677ca
Phil Ratzloff <pratzl@outlook.com>
2026-06-28 16:36:11 -0400

    Edit for D3130 Container Interface changes to sync with current implementation

--- D3130_Container_Interface/src/concepts_edges.hpp
+++ D3130_Container_Interface/src/concepts_edges.hpp
@@ -2,15 +2,15 @@
 
 // Shared edge floor (namespace std::graph): ids only.
 template <class G, class E>
-concept basic_edge = requires(G& g, const E& e) {
-  source_id(g, e);
-  target_id(g, e);
+concept basic_edge = requires(G& g, const E& uv) {
+  source_id(g, uv); // returns vertex\_id\_t<G>
+  target_id(g, uv); // returns vertex\_id\_t<G>
 };
 
 // Adjacency-list refinement (namespace std::graph::adj\_list):
 // adds the source/target vertex descriptors.
 template <class G, class E>
-concept edge = basic_edge<G, E> && requires(G& g, const E& e) {
-  source(g, e);
-  target(g, e);
+concept edge = basic_edge<G, E> && is_edge_descriptor_v<E> && requires(G& g, const E& uv) {
+  source(g, uv); // returns vertex descriptor vertex\_t<G>
+  target(g, uv); // returns vertex descriptor vertex\_t<G>
 };
